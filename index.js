94f2d753b091f97d70566e861dcb903e1c82bbcd
Ian McFarlane <ianlmcfar@gmail.com>
2022-01-03 15:40:49 -0800

    fix port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var metadataFile = fs.readFileSync('metadata.json');
 var metadata = JSON.parse(metadataFile);
 const app = express();   
     
-app.listen(3000);    
+app.listen(process.env.PORT);    
 app.use(express.static('public'));
 app.use(cors());
   
